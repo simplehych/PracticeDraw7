c9a0434df2a94a8c8df9aa82517b9a96eee50a5a
hych <simplehych@gmail.com>
2017-11-16 10:52:02 +0800

    完成

--- app/src/main/java/com/hencoder/hencoderpracticedraw7/practice/practice03/Practice03OfObjectLayout.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw7/practice/practice03/Practice03OfObjectLayout.java
@@ -52,7 +52,13 @@ public class Practice03OfObjectLayout extends RelativeLayout {
         // 重写 evaluate() 方法，让 PointF 可以作为属性来做动画
         @Override
         public PointF evaluate(float fraction, PointF startValue, PointF endValue) {
-            return startValue;
+
+            float x = startValue.x + ((endValue.x - startValue.x) * fraction);
+            float y = startValue.y + ((endValue.y - startValue.y) * fraction);
+
+            PointF pointF = new PointF();
+            pointF.set(x, y);
+            return pointF;
         }
     }
 }
